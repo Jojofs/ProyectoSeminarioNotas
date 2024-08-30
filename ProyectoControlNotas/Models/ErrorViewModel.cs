4fd66319deb0d713dfc1cec54260181897c4a26f
Jonathán Florián <jojofs.uwu@gmail.com>
2024-08-29 21:09:26 -0600

    Base de proyecto
    Se subió el proyecto a nuevo repositorio

---
+++ ProyectoControlNotas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoControlNotas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ ProyectoControlNotas/Models/LoginDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ProyectoControlNotas.Models
+{
+
+    public class LoginDbContext : IdentityDbContext
+       
+        
+    {
+        public LoginDbContext(DbContextOptions<LoginDbContext> Options) : base(Options)
+        {
+
+        }
+    }
+}
